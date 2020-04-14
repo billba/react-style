c7c27bf409e56897f0643f1fb21cf2287e84e513
Bill Barnes <billba@microsoft.com>
2020-04-14 07:42:59 -0700

    simplify, add alternate dispatch syntax

--- src/redux.ts
+++ src/redux.ts
@@ -1,23 +1,21 @@
-import {Dispatch, Reducer as _Reducer, createStore, combineReducers} from 'redux';
+import {Dispatch, Reducer as _Reducer, createStore, combineReducers, Store} from 'redux';
 import {useDispatch as _useDispatch, useSelector as _useSelector, Provider as _Provider} from 'react-redux';
 import {createElement} from 'react';
 
 import {_AppAction} from '@actions';
 import {allReducers} from './reducers';
 
+const store = createStore(
+  combineReducers(allReducers)
+);
+
+export type AppState = typeof store extends Store<infer S> ? S : never;
+
 type Typify<A> = A extends Record<infer T, any> ? T extends string ? { type: T } & A[T] : never : never;
 
 export type AppAction = Typify<_AppAction>;
 
-export type UseDispatch = Dispatch<AppAction>;
-
 export const useDispatch = () => _useDispatch<Dispatch<AppAction>>();
-
-type StateFromReducers<T> = {
-  [K in keyof T]: T[K] extends (...args: any[]) => infer R ? R : never
-}
-
-export type AppState = StateFromReducers<typeof allReducers>;
 
 export const useSelector = <TSelected>(
   selector: (state: AppState) => TSelected,
@@ -26,10 +24,8 @@ export const useSelector = <TSelected>(
 
 export type Reducer<S> = _Reducer<S, AppAction>;
 
-type OneAction<A, K> = A extends { type: K } ? A: never;
-
 export type Reducers<S> = Partial<{
-  [K in AppAction['type']]: (state: S, action: OneAction<AppAction, K>) => S;
+  [K in keyof _AppAction]: (state: S, action: _AppAction[K] & { type: K }) => S;
 }>
 
 export const reduce = <S>(reducers: Reducers<S>, state: S, action: AppAction) => {
@@ -38,14 +34,30 @@ export const reduce = <S>(reducers: Reducers<S>, state: S, action: AppAction) =>
 }
 
 export const createReducer = <S>(initState: () => S, reducers: Reducers<S>) =>
-  (state = initState(), action: AppAction) => reduce(reducers, state, action)
-
-const store = createStore(
-  combineReducers(allReducers)
-);
+  (state = initState(), action: AppAction) => reduce(reducers, state, action);
 
 export const Provider = ({children}: React.PropsWithChildren<{}> ) =>
   createElement(_Provider, {store}, children);
 
 export {allReducers}
 export {shallowEqual} from 'react-redux';
+
+// The below enables an alternate take on dispatch of the form:
+//    dispatch('typeWithNoPayload'); // 
+//    dispatch('typeWithPayload', {various: 'kinds', of: 'payload'});
+
+type NoPayload<T> = {
+  [K in keyof T]: keyof T[K] extends never ? K : never
+}[keyof T];
+
+type Payload<T> = Exclude<keyof _AppAction, NoPayload<_AppAction>>;
+
+interface NewDispatch {
+  (type: NoPayload<_AppAction>): void;
+  (type: Payload<_AppAction>, payload: _AppAction[typeof type]): void;
+}
+
+export const useAlternateDispatch = () => {
+  const dispatch = _useDispatch<Dispatch<AppAction>>();
+  return ((type: any, payload?: any) => dispatch({ type, ... payload})) as NewDispatch;
+}
